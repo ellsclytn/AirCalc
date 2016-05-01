ab400b875106310a59612c0ff587a1493dcc320b
Ellis Clayton <ellis@ellisclayton.info>
2016-05-01 14:21:03 +1000

    Add Hours & Minutes addition

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     $('#hobbsDecimal').val(moment.duration(output, 'hours').format('h [hrs]', 2))
     $('#hobbsHm').val(moment.duration(output, 'hours').format('h:mm'))
   })
+
+  $('#hm1, #hm2').keyup(function(e) {
+    var output = moment.duration($('#hm1').val(), 'hours').add(moment.duration($('#hm2').val()))
+    $('#hmOutD').val(output.format('h [hrs]', 2))
+    $('#hmOutHm').val(output.format('h:mm'))
+  })
 })
