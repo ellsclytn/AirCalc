c5d9f1be49568c636c34d58c4c34ada29a71ed6c
Ellis Clayton <ellis@ellisclayton.info>
2016-05-02 09:34:17 +1000

    Allow mixed format operations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
-  $('#hobbs1, #hobbs2').keyup(function(e) {
-    var output = Math.round(Math.abs($('#hobbs1').val() - $('#hobbs2').val()) * 10) / 10
-    $('#hobbsDecimal').val(moment.duration(output, 'hours').format('h [hrs]', 2))
-    $('#hobbsHm').val(moment.duration(output, 'hours').format('h:mm'))
-  })
+  $('#timeIn1, #timeIn2').keyup(function() {
+    var time1 = parseDuration($('#timeIn1').val())
+    var time2 = parseDuration($('#timeIn2').val())
 
-  $('#hm1, #hm2').keyup(function(e) {
-    var output = moment.duration($('#hm1').val(), 'hours').add(moment.duration($('#hm2').val()))
-    $('#hmOutD').val(output.format('h [hrs]', 2))
-    $('#hmOutHm').val(output.format('h:mm'))
+    if (time1 && time2) {
+      var output;
+      if ($('#operator').prop('checked')) {
+        output = time1.add(time2)
+      } else {
+        output = time1.subtract(time2)
+      }
+
+      $('#timeOutD').val(output.format('h [hrs]', 2))
+      $('#timeOutHm').val(output.format('h:mm'))
+    }
   })
 })
+
+function parseDuration(input) {
+  if (input.indexOf(':') > -1 && input.indexOf('.') === -1) {
+    return moment.duration(input, 'hours')
+  } else if ($.isNumeric(input)) {
+    return moment.duration(
+      moment.duration(
+        Math.round(input * 10) / 10, 'hours')
+      .format('h:mm'))
+  } else {
+    return false
+  }
+}
