3c7218c4e42017c4c602fb4fe86dd5641ff93cde
Ellis Clayton <ellis@ellisclayton.info>
2016-05-01 19:24:16 +1000

    Fix gulfile not watching Sass changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ var browserSync = require('browser-sync').create()
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass())
     .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
 })
 
 gulp.task('browser-sync', function() {
@@ -14,8 +15,11 @@ gulp.task('browser-sync', function() {
       baseDir: './'
     }
   })
+
+  gulp.watch('./**/*.scss', ['sass'])
+  gulp.watch('./index.html').on('change', browserSync.reload)
 })
 
-gulp.task('default', ['sass', 'browser-sync'], function() {
+gulp.task('default', ['browser-sync'], function() {
   gulp.watch('./sass/**/*.scss', ['sass'])
 })
